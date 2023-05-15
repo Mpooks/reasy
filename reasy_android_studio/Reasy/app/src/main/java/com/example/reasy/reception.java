310f610c05b178aabf44bb57e522f310f7ade236
ThChTa <thomas.tamv2001@hotmail.com>
2023-05-15 17:16:35 +0300

    first classes to start the code

---
+++ reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/reception.java
@@ -0,0 +1,12 @@
+package com.example.reasy;
+
+public class reception {
+    private int guest_number;
+    private int reception_id;
+    private int customer_id;
+    private String date;
+    private int artist_id;
+    private int reception_area_id;
+    private int catering_id;
+
+}
---
+++ reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/reception_area.java
@@ -0,0 +1,11 @@
+package com.example.reasy;
+
+import java.util.ArrayList;
+
+public class reception_area {
+    private String name;
+    private int reception_area_id;
+    private int num_of_guests;
+    private ArrayList<reception> receptions = new ArrayList<reception>();
+    private float cost;
+}
