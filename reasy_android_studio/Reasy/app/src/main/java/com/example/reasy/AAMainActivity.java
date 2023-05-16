4d6d0c7df06a26679cf6b98be756737db6a485e1
ThChTa <thomas.tamv2001@hotmail.com>
2023-05-16 19:07:22 +0300

    update SDK and tests

---
+++ reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/AAMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.reasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AAMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aamain);
+    }
+}
