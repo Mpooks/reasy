36a67174a0fef94d19f626deb89186b1f73bff12
ValiaDourou <valiadourou@gmail.com>
2023-05-18 20:13:18 +0300

    :)

--- reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/main_page.java
+++ reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/main_page.java
@@ -1,18 +1,40 @@
 package com.example.reasy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class main_page extends AppCompatActivity {
 
-    public void createInvitation(){}
+    public void createInvitation(View v){
+        Intent intent=new Intent(this,active_receptions_page.class);
+        startActivity(intent);
+    }
     public void show(){}
-    public void rate(){}
+    public void rate(View v){
+        Intent intent=new Intent(this,rating_form_page.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+    }
+    public void createRes(View v){
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    } //edw prepei na se paei se katasthmata
+
+    public void createRec(View v){
+        Intent intent=new Intent(this,organize_reception_page.class);
+        startActivity(intent);
+    }
+
+    public void order(View v){
+        Intent intent=new Intent(this,order_page.class);
+        startActivity(intent);
     }
 }
--- reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/shop_main_page.java
+++ reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/shop_main_page.java
@@ -2,11 +2,27 @@ package com.example.reasy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class shop_main_page extends AppCompatActivity {
 
-    public void createSupply(){}
+    public void createSupply(View v){
+        Intent intent=new Intent(this,supplier_list_page.class);
+        startActivity(intent);
+    }
+
+    public void manageStaff(View v){
+        Intent intent=new Intent(this,shop_calendar_page.class);
+        startActivity(intent);
+    }
+
+    public void addJobOffer(View v){
+        Intent intent=new Intent(this,job_offer_form_page.class);
+        startActivity(intent);
+    }
+
     public void show(){}
 
     @Override
