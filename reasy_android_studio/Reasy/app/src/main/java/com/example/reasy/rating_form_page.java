d299642bd519446401d7f2ee5b7f5a43b912ce86
Mpooks <jimbalafas2001@gmail.com>
2023-05-22 19:57:04 +0300

    a

--- reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/rating_form_page.java
+++ reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/rating_form_page.java
@@ -1,17 +1,79 @@
 package com.example.reasy;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class rating_form_page extends AppCompatActivity {
 
+    private int id,sid,numofr;
+    private double rate;
+    private ArrayList<shop> slist=new ArrayList<>();
+    private ArrayList<String> oh;
+    private TextView text3,text4,text5,text6;
+    private LinearLayout linearLayout;
+    private menu m;
+    private ArrayList<product_menu> arrayList;
+    private main_lists ml;
+    private shop chosens;
+    private String n,a,p;
     public rating_form_page() {
 
     }
     public void chooseShopAndFillRating(){
+            ml= main_lists.createLists();
+            slist = (ArrayList<shop>) ml.getShop_list().clone();
+            Bundle bundle = getIntent().getExtras();
+            id= bundle.getInt("id");
+            sid= bundle.getInt("sid");
+            for(shop s: slist){
+                if(s.getId()==sid){
+                    n=s.getName();
+                    a=s.getAddress();
+                    p=s.getPhone();
+                    m = s.getShop_m();
+                    numofr=s.getNumofrates();
+                    rate=s.getRating();
+                    arrayList=(ArrayList<product_menu>) m.getProducts().clone();
+                    oh=(ArrayList<String>) s.getOpeningHours();
+                    chosens=s;
+                }
+            }
+            linearLayout = findViewById(R.id.linear_layout);
+            text3 = findViewById(R.id.textView3);
+            text3.setText(n);
+            text4 = findViewById(R.id.textView4);
+            text4.setText("Address: "+a);
+            text5 = findViewById(R.id.textView5);
+            text5.setText("Telephone: "+p);
+            text6 = findViewById(R.id.rateg);
+            text6.setText(rate+ " ("+numofr+")");
 
+        RatingBar ratingBar;
+        Button btSubmit;
+
+        ratingBar = findViewById(R.id.ratingBar);
+        btSubmit = findViewById(R.id.rate);
+
+        btSubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                double a = (double)ratingBar.getRating();
+                chosens.changeRating(a);
+
+            }
+        });
     }
     public void show(){
         Intent intent=new Intent(this,rating_previous_orders_page.class);
@@ -21,5 +83,15 @@ public class rating_form_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating_form_page);
+        chooseShopAndFillRating();
+    }
+
+    public void goBack(View v){
+        Intent intent=new Intent(this,search_page.class);
+        Bundle b = new Bundle();
+        //Add your data to bundle
+        b.putInt("id",id);
+        intent.putExtras(b);
+        startActivity(intent);
     }
 }
---
+++ reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/shop_list_page.java
@@ -0,0 +1,98 @@
+package com.example.reasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class shop_list_page extends AppCompatActivity {
+    private int id,sid;
+    private main_lists ml;
+    private ArrayList<user> ul=new ArrayList<>();
+    private ArrayList<reservation> arrayList;
+    private LinearLayout linearLayout;
+    private ArrayList<reservation> rl=new ArrayList<reservation>();
+    private ArrayList<shop> slist=new ArrayList<>();
+    private String sname;
+
+    public void showPrevReservations(){
+        linearLayout = findViewById(R.id.linear_layout);
+        ml= main_lists.createLists();
+        ul = (ArrayList<user>) ml.getUser_list().clone();
+        slist = (ArrayList<shop>) ml.getShop_list().clone();
+        for(user u: ul) {
+            if(u.getId()==id){
+                rl=u.getReservations();
+            }
+        }
+        for(reservation r: rl)
+        {
+            Button b = new Button(this);
+            sid=r.getShop_id();
+            for(shop s: slist){
+                if(s.getId()==sid){
+                    sname=s.getName();
+                }
+            }
+
+            b.setText("Reservation id: "+r.getReservation_id()+"\nShop: "+sname);
+            b.setTextSize(18);
+            b.setHeight(192);
+            b.setWidth(966);
+            b.setPadding(30, 90, 30, 90);
+            b.setId(r.getReservation_id());
+            b.setGravity(Gravity.CENTER);
+            b.setBackgroundResource(R.drawable.menu_item);
+            Typeface typeface = getResources().getFont(R.font.seoulhangang_cbl_regular);
+            b.setTypeface(typeface);
+            b.setAllCaps(false);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            lp.setMargins(20, 0, 20, 30);
+            b.setLayoutParams(lp);
+
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent=new Intent(shop_list_page.this,rating_form_page.class);
+                    sid=b.getId();
+                    //Create the bundle
+                    Bundle b = new Bundle();
+                    //Add your data to bundle
+                    b.putInt("sid",sid);
+                    b.putInt("id",id);
+                    intent.putExtras(b);
+                    startActivity(intent);
+                }
+            });
+            linearLayout.addView(b);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Bundle bundle = getIntent().getExtras();
+        id= bundle.getInt("id");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_list_page);
+        showPrevReservations();
+    }
+
+    public void goBack(View v){
+        Intent intent=new Intent(this,main_page.class);
+        Bundle b = new Bundle();
+        //Add your data to bundle
+        b.putInt("id",id);
+        intent.putExtras(b);
+        startActivity(intent);
+    }
+}
+
