ef94c7fe37172cef452dd5c015e840f89fb77563
ThChTa <thomas.tamv2001@hotmail.com>
2023-05-24 12:36:46 +0300

    code v1

--- reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/active_receptions_page.java
+++ reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/active_receptions_page.java
@@ -1,13 +1,15 @@
 package com.example.reasy;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class active_receptions_page extends AppCompatActivity {
 
     public void invite(){}
+
     public void show(){}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
