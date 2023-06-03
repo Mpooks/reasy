b35cca6372ea11599cba9623d1cde42d70e20b82
ValiaDourou <valiadourou@gmail.com>
2023-06-03 04:29:26 +0300

    :(

--- reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/set_salary_page.java
+++ reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/set_salary_page.java
@@ -1,22 +1,45 @@
 package com.example.reasy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class set_salary_page extends AppCompatActivity {
 
     private int id;
+    private double salary,budget;
     private double[] iog=new double[3];
     private String position;
-    public void fillSalary(){
-
+    private TextView t1,t2,t3,w;
+    private EditText s;
+    public void fillSalary(View v){
+        salary=Integer.valueOf(s.getText().toString());
+        if(salary<=0) {
+            w.setText("The salary must be a positive number.");
+        }
+        else{
+           calcBudget();
+        }
     }
     public void calcBudget(){
-
+        budget=iog[0]-(iog[1]+salary);
+        show();
     }
     public void show(){
-
+        Intent intent=new Intent(this,shop_budget_jo_duration_page.class);
+        Bundle b = new Bundle();
+        //Add your data to bundle
+        b.putInt("id",id);
+        b.putString("position",position);
+        b.putDoubleArray("iog",iog);
+        b.putDouble("salary",salary);
+        b.putDouble("budget",budget);
+        intent.putExtras(b);
+        startActivity(intent);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +47,26 @@ public class set_salary_page extends AppCompatActivity {
         setContentView(R.layout.activity_set_salary_page);
         Bundle bundle = getIntent().getExtras();
         id= bundle.getInt("id");
+        iog=bundle.getDoubleArray("iog");
+        position=bundle.getString("position");
+        t1=findViewById(R.id.in);
+        t2=findViewById(R.id.ex);
+        t3=findViewById(R.id.g);
+        w=findViewById(R.id.name10);
 
+        s=findViewById(R.id.sal);
+
+        t1.setText(""+iog[0]);
+        t2.setText(""+iog[1]);
+        t3.setText(""+iog[2]);
+
+    }
+    public void goBack(View v){
+        Intent intent=new Intent(this,job_offer_form_page.class);
+        Bundle b = new Bundle();
+        //Add your data to bundle
+        b.putInt("id",id);
+        intent.putExtras(b);
+        startActivity(intent);
     }
 }
--- reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/shop_budget_jo_duration_page.java
+++ reasy_android_studio/Reasy/app/src/main/java/com/example/reasy/shop_budget_jo_duration_page.java
@@ -1,18 +1,42 @@
 package com.example.reasy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class shop_budget_jo_duration_page extends AppCompatActivity {
 
-    public shop_budget_jo_duration_page() {
-    }
-    public void fillDate(){
+    private TextView t1;
+    private int id,foundm=0;
+    private double salary,budget;
+    private double[] iog=new double[3];
+    private String position;
+    private int stay,stam,stad,endy,endm,endd;
+    private EditText sty,stm,std,eny,enm,end;
+    private CheckBox oc,ipc;
 
-    }
-    public void finalJobOffer(){
-
+    public void fillDate() {
+        if (!(oc.isChecked()) && (!(ipc.isChecked()))) {
+            foundm = 1;
+            t1.setText("You must choose one of the options.");
+        } else if ((oc.isChecked()) && ((ipc.isChecked()))) {
+            foundm = 1;
+            t1.setText("You must choose only one.");
+        }
+        if(foundm==0){
+            stay=Integer.valueOf(sty.getText().toString());
+            stam=Integer.valueOf(sty.getText().toString());
+            stad=Integer.valueOf(sty.getText().toString());
+            endy=Integer.valueOf(sty.getText().toString());
+            endm=Integer.valueOf(sty.getText().toString());
+            endd=Integer.valueOf(sty.getText().toString());
+            
+        }
     }
     public void show(){
 
@@ -21,6 +45,30 @@ public class shop_budget_jo_duration_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_budget_jo_duration_page);
-
+        Bundle bundle = getIntent().getExtras();
+        id= bundle.getInt("id");
+        iog=bundle.getDoubleArray("iog");
+        position=bundle.getString("position");
+        salary=bundle.getDouble("salary");
+        budget=bundle.getDouble("budget");
+        oc=findViewById(R.id.y);
+        ipc=findViewById(R.id.n);
+        t1=findViewById(R.id.name9);
+        sty=findViewById(R.id.sy);
+        stm=findViewById(R.id.sm);
+        std=findViewById(R.id.sd);
+        eny=findViewById(R.id.ey);
+        enm=findViewById(R.id.em);
+        end=findViewById(R.id.ed);
     }
+    public void show(View v){
+            Intent intent=new Intent(this,set_salary_page.class);
+            Bundle b = new Bundle();
+            //Add your data to bundle
+            b.putInt("id",id);
+            b.putString("position",position);
+            b.putDoubleArray("iog",iog);
+            intent.putExtras(b);
+            startActivity(intent);
+        }
 }
